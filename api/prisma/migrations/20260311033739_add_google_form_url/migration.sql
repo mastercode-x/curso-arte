f4e15b7cb5f509d9c383fae89877444cdad1f50f
jony <jonyxdxp@gmail.com>
2026-03-11 00:37:51 -0300

    fix: add googleFormUrl to schema and migration

---
+++ api/prisma/migrations/20260311033739_add_google_form_url/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `google_form_url` on the `configuracion_profesor` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "configuracion_profesor" DROP COLUMN "google_form_url";

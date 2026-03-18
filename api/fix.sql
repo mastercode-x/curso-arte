06419d3e8b08f0e156cd81a3abf6ce6093b0d47d
jony <jonyxdxp@gmail.com>
2026-03-18 11:29:34 -0300

    2

--- api/fix.sql
+++ api/fix.sql
@@ -1,2 +1,1 @@
-ALTER TABLE "configuracion_profesor" ADD COLUMN IF NOT EXISTS "instagram_url" TEXT;
-ALTER TABLE "configuracion_profesor" ADD COLUMN IF NOT EXISTS "facebook_url" TEXT;
+UPDATE "_prisma_migrations" SET "finished_at" = NOW(), "logs" = NULL WHERE "migration_name" = '20260311141907_add_calendar_table';
